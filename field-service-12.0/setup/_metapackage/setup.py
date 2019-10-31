ee56d6ac49030780d18eeccf75c9111dc540204a
Awahuang <awahuang@hotmail.com>
2019-10-31 17:10:00 +0800

    field_service

---
+++ field-service-12.0/setup/_metapackage/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-field-service",
+    description="Meta package for oca-field-service Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-fieldservice',
+        'odoo12-addon-fieldservice_account',
+        'odoo12-addon-fieldservice_agreement',
+        'odoo12-addon-fieldservice_crm',
+        'odoo12-addon-fieldservice_delivery',
+        'odoo12-addon-fieldservice_distribution',
+        'odoo12-addon-fieldservice_geoengine',
+        'odoo12-addon-fieldservice_isp_flow',
+        'odoo12-addon-fieldservice_maintenance',
+        'odoo12-addon-fieldservice_partner_multi_relation',
+        'odoo12-addon-fieldservice_project',
+        'odoo12-addon-fieldservice_purchase',
+        'odoo12-addon-fieldservice_recurring',
+        'odoo12-addon-fieldservice_repair',
+        'odoo12-addon-fieldservice_sale',
+        'odoo12-addon-fieldservice_sale_recurring',
+        'odoo12-addon-fieldservice_skill',
+        'odoo12-addon-fieldservice_stage_server_action',
+        'odoo12-addon-fieldservice_stock',
+        'odoo12-addon-fieldservice_stock_account',
+        'odoo12-addon-fieldservice_stock_account_analytic',
+        'odoo12-addon-fieldservice_substatus',
+        'odoo12-addon-fieldservice_vehicle',
+        'odoo12-addon-fieldservice_vehicle_stock',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
