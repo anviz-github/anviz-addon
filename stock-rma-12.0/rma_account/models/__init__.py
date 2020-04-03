de309bbb73248d21c569d8458df0fae4eb11c5a8
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-04-03 17:42:41 +0800

    rma

--- stock-rma-12.0/rma_account/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-
-from . import rma_order
-from . import rma_order_line
-from . import rma_operation
-from . import invoice
--- stock-rma-12.0/rma_repair/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-
-from . import rma_order_line_make_repair
